754bdb9d9d7db7854babb579b7414ba8103d74e1
Ryan Gamboa <gabe.gamboa05@gmail.com>
2024-09-30 22:36:29 -0700

    implemented my solution to the rewriting of the sumOfTripledEvens function and added the solution provided by TOP

--- objectBasics.js
+++ objectBasics.js
@@ -1,29 +1,33 @@
-function sumOfTripledEvens(array) {
-    let sum = 0;
-    for (let i = 0; i < array.length; i++) {
-        // If element is an even number 
-        if (array[i] % 2 ==0) {
-            const tripleEvenNumber = array[i] * 3;
-            // Multiply this number by three 
+//Rewriting sumofTripledEvens with new methods
 
-            // Add the new number to the total
-            sum += tripleEvenNumber;
-        }
-    }
-    return sum;
-}
-
-function addOne(num) {
-    return num + 1;
-}
 const arr = [1,2,3,4,5];
-const mappedArr = arr.map((num) => num + 1);
-console.log(mappedArr); // Outputs [2,3,4,5,6]
-console.log(arr); // Outputs [1,2,3,4,5]
 
-function isOdd(num) {
-    return num % 2 !== 0;
+function isEven(num) {
+    return num % 2 === 0;
 }
-const oddNums = arr.filter(isOdd); // iterate through arr and pass every val into isOdd function, one at a time
-console.log(oddNums);
-console.log(arr);
+
+function multiplyByThree(num) {
+    return num * 3;
+}
+
+function sumOfTripledEvens(array) {
+    isEven(array); // checks if num in arr is even
+    console.log(array);
+    
+    const multipliedArr = array.map(multiplyByThree); // mulitplies elements of array by 3
+    console.log(multipliedArr);
+
+    const sumOfTripledEvens = array.reduce((total, currentItem) => {
+        return total + currentItem;
+    }, 1);
+
+    console.log(sumOfTripledEvens);
+}
+
+// Actual Solution 
+function sumOfTripledEvens(array) {
+    return array
+      .filter((num) => num % 2 === 0)
+      .map((num) => num * 3)
+      .reduce((acc, curr) => acc + curr);
+  }
